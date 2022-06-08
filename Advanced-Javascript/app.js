b848740c7c5668b156ee8017e894c7d810171084
Divya Paulson <divyapaulsont@gmail.com>
2022-06-08 10:55:29 +0530

    advanced javascript

---
+++ Advanced-Javascript/app.js
@@ -0,0 +1,10 @@
+// console.log(document);
+// console.dir(document);
+//document.body.children[1].children[0].href = 'https://google.com';
+//alert();
+//window.alert();
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('p a');
+anchorElement.href = 'https://facebook.com';
