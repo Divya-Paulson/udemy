7c9c9a8cdcf715d9e189089a6b34aa79aa8293fa
Divya Paulson <divyapaulsont@gmail.com>
2022-07-14 10:41:16 +0530

    changes in edit button

--- Tic-Tac Game Project/scripts/config.js
+++ Tic-Tac Game Project/scripts/config.js
@@ -1,15 +1,28 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid;
+
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
-
 }
 
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
-
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = ' ';
 }
 
-function savePlayerConfig() {
+function savePlayerConfig(event) {
 
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayerName = formData.get('playername').trim();
+
+    if (!enteredPlayerName) //enteredPlayerName is just blankspace
+    {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid name!!'
+        return;
+    }
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
 }
