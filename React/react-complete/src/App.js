9cb8661bb30381d0a3bb1c7e828361369f70fd95
Divya Paulson <divyapaulsont@gmail.com>
2022-08-15 16:24:04 +0530

    added new components

--- React/react-complete/src/App.js
+++ React/react-complete/src/App.js
@@ -1,6 +1,6 @@
 import Expenses from'./components/Expenses/Expenses';
 
-
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
 
@@ -34,7 +34,7 @@ function App() {
 
     return (
     <div>
-        <h2> Let 's get started!</h2>   
+        <NewExpense />
         <Expenses expenses={expenses}/>
         </div>
     );
